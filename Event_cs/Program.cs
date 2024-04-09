ec537186bd0e293ad07e0b25f3c455dfcaceb193
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 16:39:08 +0300

    initiall commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Event_cs
+﻿using System;
+
+namespace IntegerExtensions
 {
-    internal class Program
+    public static class IntegerExtensions
+    {
+        public static bool IsPrime(this int number)
+        {
+            if (number <= 1)
+                return false;
+            if (number == 2)
+                return true;
+            if (number % 2 == 0)
+                return false;
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = 17;
+
+            if (number.IsPrime())
+            {
+                Console.WriteLine($"{number} is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a prime number.");
+            }
         }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,29 @@
-namespace TestEvent
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntegerExtensions;
+
+namespace IntegerExtensions.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class IntegerExtensionsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsPrime_PrimeNumber_ReturnsTrue()
         {
+            int primeNumber = 17;
+
+            bool isPrime = primeNumber.IsPrime();
+
+            Assert.IsTrue(isPrime);
+        }
+
+        [TestMethod]
+        public void IsPrime_NonPrimeNumber_ReturnsFalse()
+        {
+            int nonPrimeNumber = 20;
+
+            bool isPrime = nonPrimeNumber.IsPrime();
+
+            Assert.IsFalse(isPrime);
         }
     }
 }

afb018cddb85c4f6a97ccfc6811eb65a38546175
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 16:16:55 +0300

    initiall commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Event_cs
+﻿using System;
+
+namespace IntegerExtensions
 {
-    internal class Program
+    public static class IntegerExtensions
+    {
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = 10;
+            Console.WriteLine($"Is {number} even? {number.IsEven()}");
+
+            number = 7;
+            Console.WriteLine($"Is {number} even? {number.IsEven()}");
         }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,30 @@
-namespace TestEvent
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntegerExtensions;
+
+namespace IntegerExtensions.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class IntegerExtensionsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsEven_EvenNumber_ReturnsTrue()
         {
+            int number = 10;
+
+            bool result = number.IsEven();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsEven_OddNumber_ReturnsFalse()
+        {
+            int number = 7;
+
+            bool result = number.IsEven();
+
+            Assert.IsFalse(result);
         }
     }
 }

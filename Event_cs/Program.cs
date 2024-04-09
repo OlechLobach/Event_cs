a6faa0d5d7aa1c968434064d34fbff953f97b6d8
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 16:49:59 +0300

    initiall commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Event_cs
+﻿using System;
+
+namespace StringExtensions
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static int CountVowels(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0;
+            }
+
+            int count = 0;
+            foreach (char c in str.ToLower())
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = "Hello, World!";
+            int vowelCount = input.CountVowels();
+            Console.WriteLine($"Number of vowels in the string: {vowelCount}");
         }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,49 @@
-namespace TestEvent
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringExtensions;
+
+namespace StringExtensions.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class StringExtensionsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CountVowels_EmptyString_ReturnsZero()
         {
+            string input = "";
+
+            int count = input.CountVowels();
+
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod]
+        public void CountVowels_NoVowels_ReturnsZero()
+        {
+            string input = "12345";
+
+            int count = input.CountVowels();
+
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod]
+        public void CountVowels_SingleVowel_ReturnsOne()
+        {
+            string input = "a";
+
+            int count = input.CountVowels();
+
+            Assert.AreEqual(1, count);
+        }
+
+        [TestMethod]
+        public void CountVowels_MultipleVowels_ReturnsCorrectCount()
+        {
+            string input = "Hello, World!";
+
+            int count = input.CountVowels();
+
+            Assert.AreEqual(3, count);
         }
     }
 }

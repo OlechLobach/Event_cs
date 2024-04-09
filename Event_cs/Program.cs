b86bc74dfd39518139ce1cadf90b880e174c37d3
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 16:56:29 +0300

    initiall commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Event_cs
+﻿using System;
+
+namespace StringExtensions
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static int CountConsonants(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return 0;
+
+            int consonantCount = 0;
+            foreach (char c in str.ToLower())
+            {
+                if (char.IsLetter(c) && !"aeiou".Contains(c))
+                {
+                    consonantCount++;
+                }
+            }
+
+            return consonantCount;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = "Hello, World!";
+            int consonantCount = input.CountConsonants();
+            Console.WriteLine($"Number of consonants in '{input}': {consonantCount}");
         }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,49 @@
-namespace TestEvent
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringExtensions;
+
+namespace StringExtensions.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class StringExtensionsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CountConsonants_EmptyString_ReturnsZero()
         {
+            string input = "";
+
+            int count = input.CountConsonants();
+
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod]
+        public void CountConsonants_NoConsonants_ReturnsZero()
+        {
+            string input = "aeiou";
+
+            int count = input.CountConsonants();
+
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod]
+        public void CountConsonants_MixedString_ReturnsCorrectCount()
+        {
+            string input = "Hello, World!";
+
+            int count = input.CountConsonants();
+
+            Assert.AreEqual(7, count);
+        }
+
+        [TestMethod]
+        public void CountConsonants_NullString_ReturnsZero()
+        {
+            string input = null;
+
+            int count = input.CountConsonants();
+
+            Assert.AreEqual(0, count);
         }
     }
 }

b8c2d787db3d12e19a7e8c663d3628e5f30a199d
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 14:37:53 +0300

    initialll commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Event_cs
+﻿using System;
+
+namespace StringExtensions
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static int LengthOfLastWord(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return 0;
+            }
+
+            string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length == 0)
+            {
+                return 0;
+            }
+
+            return words[words.Length - 1].Length;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = "This is a sample sentence";
+            int length = text.LengthOfLastWord();
+            Console.WriteLine($"Length of last word: {length}");
         }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,59 @@
-namespace TestEvent
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringExtensions.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class StringExtensionTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LengthOfLastWord_EmptyString_ReturnsZero()
         {
+            string input = "";
+
+            int length = input.LengthOfLastWord();
+
+            Assert.AreEqual(0, length);
+        }
+
+        [TestMethod]
+        public void LengthOfLastWord_SingleWord_ReturnsLength()
+        {
+            string input = "Hello";
+
+            int length = input.LengthOfLastWord();
+
+            Assert.AreEqual(5, length);
+        }
+
+        [TestMethod]
+        public void LengthOfLastWord_MultipleWords_ReturnsLengthOfLastWord()
+        {
+            string input = "This is a test sentence";
+
+            int length = input.LengthOfLastWord();
+
+            Assert.AreEqual(8, length);
+        }
+
+        [TestMethod]
+        public void LengthOfLastWord_ExtraSpaces_ReturnsLengthOfLastWord()
+        {
+            string input = "   There   are    extra  spaces   ";
+
+            int length = input.LengthOfLastWord();
+
+            Assert.AreEqual(6, length);
+        }
+
+        [TestMethod]
+        public void LengthOfLastWord_NullString_ReturnsZero()
+        {
+            string input = null;
+
+            int length = input.LengthOfLastWord();
+
+            Assert.AreEqual(0, length);
         }
     }
 }

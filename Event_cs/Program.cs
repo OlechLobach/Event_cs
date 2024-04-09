f9644ad8e6334cc933b12fc2f63abe2367f826b7
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 14:41:46 +0300

    initiall commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Event_cs
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringExtensions
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static bool IsValidParentheses(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return true;
+
+            var stack = new Stack<char>();
+            var mappings = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '{', '}' },
+                { '[', ']' }
+            };
+
+            foreach (char c in str)
+            {
+                if (mappings.ContainsKey(c))
+                {
+                    stack.Push(c);
+                }
+                else if (mappings.ContainsValue(c))
+                {
+                    if (stack.Count == 0 || mappings[stack.Pop()] != c)
+                        return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input1 = "{}[]";
+            Console.WriteLine($"IsValidParentheses for '{input1}': {input1.IsValidParentheses()}");
+
+            string input2 = "[}]";
+            Console.WriteLine($"IsValidParentheses for '{input2}': {input2.IsValidParentheses()}");
+
+            string input3 = "[[{]}]";
+            Console.WriteLine($"IsValidParentheses for '{input3}': {input3.IsValidParentheses()}");
         }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,58 @@
-namespace TestEvent
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringExtensions.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class StringExtensionTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsValidParentheses_EmptyString_ReturnsTrue()
         {
+            string input = "";
+
+            bool isValid = input.IsValidParentheses();
+
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        public void IsValidParentheses_ValidString_ReturnsTrue()
+        {
+            string input = "{}[]";
+
+            bool isValid = input.IsValidParentheses();
+
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        public void IsValidParentheses_ValidNestedString_ReturnsTrue()
+        {
+            string input = "[{}]";
+
+            bool isValid = input.IsValidParentheses();
+
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        public void IsValidParentheses_InvalidString_ReturnsFalse()
+        {
+            string input = "[}";
+
+            bool isValid = input.IsValidParentheses();
+
+            Assert.IsFalse(isValid);
+        }
+
+        [TestMethod]
+        public void IsValidParentheses_InvalidNestedString_ReturnsFalse()
+        {
+            string input = "[[{]}]";
+
+            bool isValid = input.IsValidParentheses();
+
+            Assert.IsFalse(isValid);
         }
     }
 }

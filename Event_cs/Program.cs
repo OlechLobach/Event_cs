fb78bab5f16d8a50e7cda2cf62af9d4c886c8bad
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 17:17:00 +0300

    initiall commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace Event_cs
+﻿using System;
+using System.Linq;
+
+namespace Point3DCalculator
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Point3D[] points = new Point3D[]
+            {
+                new Point3D { X = 0, Y = 0, Z = 0 },
+                new Point3D { X = 1, Y = 1, Z = 1 },
+                new Point3D { X = 2, Y = 2, Z = 2 },
+                new Point3D { X = 3, Y = 3, Z = 3 }
+            };
+
+            double maxDistance = double.MinValue;
+            Point3D firstPoint = null;
+            Point3D secondPoint = null;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                for (int j = i + 1; j < points.Length; j++)
+                {
+                    double distance = points[i].DistanceTo(points[j]);
+                    if (distance > maxDistance)
+                    {
+                        maxDistance = distance;
+                        firstPoint = points[i];
+                        secondPoint = points[j];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Maximum distance: {maxDistance}");
+            Console.WriteLine($"Points: ({firstPoint.X}, {firstPoint.Y}, {firstPoint.Z}) and ({secondPoint.X}, {secondPoint.Y}, {secondPoint.Z})");
+        }
+    }
+
+    public class Point3D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public double DistanceTo(Point3D other)
+        {
+            double dx = X - other.X;
+            double dy = Y - other.Y;
+            double dz = Z - other.Z;
+            return Math.Sqrt(dx * dx + dy * dy + dz * dz);
         }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,29 @@
-namespace TestEvent
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Point3DCalculator.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class Point3DTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestDistanceTo_SamePoint_ReturnsZero()
         {
+            var point = new Point3D { X = 1, Y = 2, Z = 3 };
+
+            double distance = point.DistanceTo(point);
+
+            Assert.AreEqual(0, distance);
+        }
+
+        [TestMethod]
+        public void TestDistanceTo_DifferentPoints_ReturnsCorrectDistance()
+        {
+            var point1 = new Point3D { X = 0, Y = 0, Z = 0 };
+            var point2 = new Point3D { X = 1, Y = 1, Z = 1 };
+
+            double distance = point1.DistanceTo(point2);
+
+            Assert.AreEqual(Math.Sqrt(3), distance);
         }
     }
 }

98f962d44cbe8ad97da11d652df806a999cfe33d
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 15:00:44 +0300

    initiall commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Event_cs
+﻿using System;
+using System.Linq;
+
+namespace TemperatureAnalyzer
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DaytimeTemperature[] temperatures = new DaytimeTemperature[]
+            {
+                new DaytimeTemperature { Day = 1, HighestTemperature = 25, LowestTemperature = 10 },
+                new DaytimeTemperature { Day = 2, HighestTemperature = 30, LowestTemperature = 15 },
+                new DaytimeTemperature { Day = 3, HighestTemperature = 20, LowestTemperature = 5 },
+                new DaytimeTemperature { Day = 4, HighestTemperature = 28, LowestTemperature = 12 },
+                new DaytimeTemperature { Day = 5, HighestTemperature = 22, LowestTemperature = 8 }
+            };
+
+            int maxDifference = temperatures.Max(t => t.HighestTemperature - t.LowestTemperature);
+            var daysWithMaxDifference = temperatures.Where(t => t.HighestTemperature - t.LowestTemperature == maxDifference);
+
+            Console.WriteLine($"Days with maximum difference ({maxDifference}°C):");
+            foreach (var day in daysWithMaxDifference)
+            {
+                Console.WriteLine($"Day {day.Day}");
+            }
+        }
+    }
+
+    public class DaytimeTemperature
+    {
+        public int Day { get; set; }
+        public int HighestTemperature { get; set; }
+        public int LowestTemperature { get; set; }
+
+        public int CalculateTemperatureDifference()
+        {
+            return HighestTemperature - LowestTemperature;
         }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,18 @@
-namespace TestEvent
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TemperatureAnalyzer.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class DaytimeTemperatureTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalculateTemperatureDifference_ValidInput_ReturnsCorrectDifference()
         {
+            var temperature = new DaytimeTemperature { HighestTemperature = 25, LowestTemperature = 10 };
+
+            var difference = temperature.CalculateTemperatureDifference();
+
+            Assert.AreEqual(15, difference);
         }
     }
 }

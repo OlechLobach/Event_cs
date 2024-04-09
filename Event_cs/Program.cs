2a7c1a5dc323c402e1471b7a7902055fa609efd2
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 17:08:08 +0300

    initiall commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Event_cs
+﻿using System;
+
+namespace StringExtensions
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static int CountSentences(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return 0;
+
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (c == '.' || c == '!' || c == '?')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = "This is a test sentence. Another sentence follows! And one more? Yes, one more.";
+            int count = input.CountSentences();
+            Console.WriteLine($"Number of sentences: {count}");
         }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,39 @@
-namespace TestEvent
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringExtensions;
+
+namespace StringExtensions.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class StringExtensionsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CountSentences_EmptyString_ReturnsZero()
         {
+            string input = "";
+
+            int count = input.CountSentences();
+
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod]
+        public void CountSentences_SingleSentence_ReturnsOne()
+        {
+            string input = "This is a test sentence.";
+
+            int count = input.CountSentences();
+
+            Assert.AreEqual(1, count);
+        }
+
+        [TestMethod]
+        public void CountSentences_MultipleSentences_ReturnsCorrectCount()
+        {
+            string input = "This is a test sentence. Another sentence follows! And one more? Yes, one more.";
+
+            int count = input.CountSentences();
+
+            Assert.AreEqual(4, count);
         }
     }
 }

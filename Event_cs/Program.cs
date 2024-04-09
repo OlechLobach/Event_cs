ad75e719d4e4ebf779a255c6141cff734fa0038d
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 14:46:23 +0300

    initiall commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Event_cs
+﻿using System;
+using System.Linq;
+
+namespace ArrayExtensions
 {
-    internal class Program
+    public static class ArrayExtensions
+    {
+        public static int[] Filter(this int[] array, Func<int, bool> predicate)
+        {
+            return array.Where(predicate).ToArray();
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int[] evenNumbers = numbers.Filter(x => x % 2 == 0);
+            Console.WriteLine("Even numbers: " + string.Join(", ", evenNumbers));
+
+            int[] oddNumbers = numbers.Filter(x => x % 2 != 0);
+            Console.WriteLine("Odd numbers: " + string.Join(", ", oddNumbers));
         }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,30 @@
-namespace TestEvent
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArrayExtensions;
+using System;
+
+namespace ArrayExtensions.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class ArrayExtensionsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Filter_ReturnsEvenNumbers()
         {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int[] result = numbers.Filter(x => x % 2 == 0);
+
+            CollectionAssert.AreEqual(new[] { 2, 4, 6, 8, 10 }, result);
+        }
+
+        [TestMethod]
+        public void Filter_ReturnsOddNumbers()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int[] result = numbers.Filter(x => x % 2 != 0);
+
+            CollectionAssert.AreEqual(new[] { 1, 3, 5, 7, 9 }, result);
         }
     }
 }

3e910f6f9c547b32565a844eef9c849d9bddd60e
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 14:34:06 +0300

    initiall commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Event_cs
+﻿using System;
+
+namespace StringExtensions
 {
-    internal class Program
+    public static class StringExtensions
+    {
+        public static int WordCount(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return 0;
+            }
+
+            string[] words = str.Split(new char[] { ' ', '.', ',', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = "This is a test string";
+            int count = input.WordCount();
+            Console.WriteLine($"Word count: {count}");
         }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,48 @@
-namespace TestEvent
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringExtensions.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class StringExtensionsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void WordCount_EmptyString_ReturnsZero()
         {
+            string input = "";
+
+            int count = input.WordCount();
+
+            Assert.AreEqual(0, count);
+        }
+
+        [TestMethod]
+        public void WordCount_SingleWord_ReturnsOne()
+        {
+            string input = "Hello";
+
+            int count = input.WordCount();
+
+            Assert.AreEqual(1, count);
+        }
+
+        [TestMethod]
+        public void WordCount_MultipleWords_ReturnsCorrectCount()
+        {
+            string input = "This is a test string";
+
+            int count = input.WordCount();
+
+            Assert.AreEqual(5, count);
+        }
+
+        [TestMethod]
+        public void WordCount_ExtraSpaces_ReturnsCorrectCount()
+        {
+            string input = "   There   are    extra  spaces   ";
+
+            int count = input.WordCount();
+
+            Assert.AreEqual(4, count);
         }
     }
 }

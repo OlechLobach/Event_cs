14da967a2c284ad4f4ee2e0b219d664b7c789c10
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-09 15:09:47 +0300

    initiall commit

--- Event_cs/Program.cs
+++ Event_cs/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Event_cs
+﻿using System;
+using System.Linq;
+
+namespace StudentGrades
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StudentGrade[] studentGrades = new StudentGrade[]
+            {
+                new StudentGrade { Name = "Alice", Grades = new int[] { 85, 90, 95, 88 } },
+                new StudentGrade { Name = "Bob", Grades = new int[] { 78, 82, 85, 80 } },
+                new StudentGrade { Name = "Charlie", Grades = new int[] { 92, 88, 95, 90 } }
+            };
+
+            var maxGradeStudent = studentGrades.OrderByDescending(s => s.Grades.Max()).First();
+            Console.WriteLine($"Student with maximum grade is {maxGradeStudent.Name} with grade {maxGradeStudent.Grades.Max()}");
+
+            var averageGradeStudent = studentGrades.OrderByDescending(s => s.Grades.Average()).First();
+            Console.WriteLine($"Student with average grade is {averageGradeStudent.Name} with grade {averageGradeStudent.Grades.Average()}");
+
+            Console.ReadKey();
         }
+    }
+
+    public class StudentGrade
+    {
+        public string Name { get; set; }
+        public int[] Grades { get; set; }
     }
 }
--- TestEvent/UnitTest1.cs
+++ TestEvent/UnitTest1.cs
@@ -1,11 +1,41 @@
-namespace TestEvent
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentGrades;
+using System.Linq;
+using TemperatureAnalyzer;
+
+namespace TemperatureAnalyzer.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class StudentGradeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalculateAverageGrade_ValidInput_ReturnsCorrectResult()
         {
+            var studentGrades = new[]
+            {
+                new StudentGrade { Name = "Alice", Grades = new int[] { 80, 85, 90 } },
+                new StudentGrade { Name = "Bob", Grades = new int[] { 75, 80, 85 } },
+                new StudentGrade { Name = "Charlie", Grades = new int[] { 70, 75, 80 } }
+            };
+
+            var averageGrades = studentGrades.Select(s => s.Grades.Average()).ToArray();
+
+            CollectionAssert.AreEqual(new[] { 85.0, 80.0, 75.0 }, averageGrades);
+        }
+
+        [TestMethod]
+        public void CalculateMaximumGrade_ValidInput_ReturnsCorrectResult()
+        {
+            var studentGrades = new[]
+            {
+                new StudentGrade { Name = "Alice", Grades = new int[] { 80, 85, 90 } },
+                new StudentGrade { Name = "Bob", Grades = new int[] { 75, 80, 85 } },
+                new StudentGrade { Name = "Charlie", Grades = new int[] { 70, 75, 80 } }
+            };
+
+            var maximumGrades = studentGrades.Select(s => s.Grades.Max()).ToArray();
+
+            CollectionAssert.AreEqual(new[] { 90, 85, 80 }, maximumGrades);
         }
     }
 }
